12fe06286e572e4f8d2e3fd1407df85bf7cddf04
mihirrana620 <84765232+mihirrana620@users.noreply.github.com>
2022-03-05 06:22:25 +0000

    Add files via upload

---
+++ Mihir_Verilog_files/Mihir_SR_Latch.v
@@ -0,0 +1,16 @@
+module Mihir_SR_Latch(Q, Qn, CLK, S, R);
+   output Q;
+   output Qn;
+   input  CLK;   
+   input  S;
+   input  R;
+
+   wire   S1;
+   wire   R1;
+   
+   and(S1, CLK, S);
+   and(R1, CLK, R);   
+   nor(Qn, S1, Q);
+   nor(Q, R1, Qn);
+endmodule
+         
